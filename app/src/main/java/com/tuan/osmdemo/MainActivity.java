84b1a0904e6bfb5ee9973bd89d4503f44ff02462
tuanpham_personal <tuan.pa90@gmail.com>
2016-10-20 02:31:10 +0700

    Show osm with current localtion

---
+++ app/src/main/java/com/tuan/osmdemo/MainActivity.java
@@ -0,0 +1,120 @@
+package com.tuan.osmdemo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.MapController;
+import org.osmdroid.api.IMapController;
+
+public class MainActivity extends Activity {
+
+    private MapView myOpenMapView;
+    private MapController myMapController;
+    LocationManager locationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //important! set your user agent to prevent getting banned from the osm servers
+        org.osmdroid.tileprovider.constants.OpenStreetMapTileProviderConstants.setUserAgentValue(BuildConfig.APPLICATION_ID);
+
+        myOpenMapView = (MapView) findViewById(R.id.map);
+        myOpenMapView.setTileSource(TileSourceFactory.MAPNIK);
+        myOpenMapView.setBuiltInZoomControls(true);
+
+        myMapController = (MapController) myOpenMapView.getController();
+        myMapController.setZoom(20);
+
+
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        //for demo, getLastKnownLocation from GPS only, not from NETWORK
+        Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if(lastLocation != null){
+            updateLoc(lastLocation);
+        }
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, myLocationListener);
+        //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, myLocationListener);
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        locationManager.removeUpdates(myLocationListener);
+    }
+
+    private void updateLoc(Location loc){
+        GeoPoint locGeoPoint = new GeoPoint(loc.getLatitude(), loc.getLongitude());
+        myMapController.setCenter(locGeoPoint);
+        myOpenMapView.invalidate();
+    }
+
+    private LocationListener myLocationListener
+            = new LocationListener(){
+
+        @Override
+        public void onLocationChanged(Location location) {
+            // TODO Auto-generated method stub
+            updateLoc(location);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // TODO Auto-generated method stub
+
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
